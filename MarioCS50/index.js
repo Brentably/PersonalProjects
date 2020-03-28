39ca701688b4e03222f2ab1dcd0dc90c8024a8a5
brent <brent@pabnet.com>
2020-03-28 18:10:02 -0400

    Started this pyramid project, have the framework laid out except for any of the actual pyramidFunction

---
+++ MarioCS50/index.js
@@ -0,0 +1,14 @@
+
+
+
+function createPyramid() {
+    let x = document.getElementById("num").value
+    pyramidDiv = document.getElementById("pyramidDiv");
+    
+    for (let j = 0; j < x; j++) {
+        pyramidDiv.innerHTML += "hello"
+    }
+
+
+    return false;
+}
