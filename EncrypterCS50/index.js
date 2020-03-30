f5c333141ccb01322d5242bc85699909e7d59148
brent <brent@pabnet.com>
2020-03-30 00:07:50 -0400

    started encrypter project

---
+++ EncrypterCS50/index.js
@@ -0,0 +1,22 @@
+
+const encrypt = () => {
+
+let output = document.getElementById("encryption");
+let alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let alphabet = alphabetString.split("");
+let keyString = document.getElementById("key").value;
+let key = keyString.split("");
+let message = document.getElementById("message").value.split("");
+let newMessage = [];
+
+
+message.forEach((element, index) => {
+    
+});
+
+
+
+
+ output.innerHTML = newMessage
+}
+
