d2ba11c262a67b29ed582764c6cb246c0d7f6c6e
brent <brent@pabnet.com>
2020-04-03 23:12:24 -0400

    tried the same thing but with table cells instead of a shit ton of divs and classes

--- QuantizedMidiplayer/index.js
+++ QuantizedMidiplayer/index.js
@@ -1,3 +1,11 @@
-const boxes = document.querySelectorAll(".col");
+const tableRows = 8
+const tableColumns = 16
+for (let j = 0; j < tableRows; j++){
+let row = document.getElementById("myTable").insertRow(j)
+for (let i = 0; i < tableColumns; i++) {
+row.insertCell(i)
+}
+}
+const boxes = document.querySelectorAll("td")
 console.log(boxes)
 boxes.forEach(box => {box.addEventListener("click", toggleSelected => box.classList.toggle("selected"))})
