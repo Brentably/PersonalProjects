5c11ecfcde72bff5c7ad59d235634613edc34fd6
brent <brent@pabnet.com>
2020-04-04 20:00:03 -0500

    added variable no. of rows / columns, can now sort by which column or row you want

--- QuantizedMidiplayer/index.js
+++ QuantizedMidiplayer/index.js
@@ -1,11 +1,19 @@
 const tableRows = 8
 const tableColumns = 16
+const table = []
 for (let j = 0; j < tableRows; j++){
 let row = document.getElementById("myTable").insertRow(j)
+table.push(row)
 for (let i = 0; i < tableColumns; i++) {
-row.insertCell(i)
+table[j].insertCell(i)
+table[j].childNodes[i].classList.add(`row${j}`);
+table[j].childNodes[i].classList.add(`column${i}`)
 }
 }
-const boxes = document.querySelectorAllq("td")
-console.log(boxes)
+
+
+
+
+
+const boxes = document.querySelectorAll("td")
 boxes.forEach(box => {box.addEventListener("click", toggleSelected => box.classList.toggle("selected"))})
