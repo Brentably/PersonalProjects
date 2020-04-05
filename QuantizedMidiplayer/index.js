616defb6b379056f8299b8e2343d8e99107f9386
brent <brent@pabnet.com>
2020-04-05 11:04:13 -0500

    miscellaneous updates

--- QuantizedMidiplayer/index.js
+++ QuantizedMidiplayer/index.js
@@ -1,5 +1,6 @@
 const tableRows = 8
 const tableColumns = 16
+//the following code block creates a table of a variable number of rows and columns based on the above constants and adds classes row# and column# classes to them, so each column or row can later be selected with a query selector
 const table = []
 for (let j = 0; j < tableRows; j++){
 let row = document.getElementById("myTable").insertRow(j)
@@ -12,8 +13,25 @@ table[j].childNodes[i].classList.add(`column${i}`)
 }
 
 
+//the following code sets of the audio for a given row. It's not that efficient. Can improve.
+const highcboxes = document.querySelectorAll(".row0")
+highcboxes.forEach(highcbox => {highcbox.addEventListener('click', sound => {
+    highc = document.querySelector("audio[data-note='highc']");
+    highc.currentTime = "0"
+    highc.play();
+})})
 
+
+
+
+
+//the following code toggles the selected class when clicked
+function toggleSelected(e) {
+    this.classList.toggle("selected")
+    console.log(this)
+    
+ }
 
 
 const boxes = document.querySelectorAll("td")
-boxes.forEach(box => {box.addEventListener("click", toggleSelected => box.classList.toggle("selected"))})
+boxes.forEach(box => {box.addEventListener('click', toggleSelected)})
