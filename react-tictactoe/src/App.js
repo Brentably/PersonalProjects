a1b0190cd6ef40c41bb4c89b2895726bbe7cdf7e
brent <brent@pabnet.com>
2020-04-22 18:54:18 -0400

    started tic tac toe

---
+++ react-tictactoe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TicTacToe from './TicTacToe';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-tictactoe/src/TicTacToe/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css'
+
+function TicTacToe() {
+    return (
+        <table>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    )
+}
+
+export default TicTacToe
