03c4e85060c136ed91b2d16924b81aa7e2c664c4
brent <brent@pabnet.com>
2020-04-23 13:12:39 -0400

    started tic tac toe game with react

--- react-tictactoe/src/TicTacToe/index.js
+++ react-tictactoe/src/TicTacToe/index.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import './style.css'
 
-function TicTacToe() {
+class Square extends React.Component {
+    constructor(props){
+        super()
+        this.props = props
+        this.state = {
+            letter: ""
+        }
+        this.mark = this.mark.bind(this)
+    }
+    mark(num) {
+        console.log(num)
+    }
+    render() {
     return (
-        <table>
+        <td onClick={() => this.mark(this.props.number)}><div>{this.state.letter}</div></td>
+    )
+    }
+}
+
+class TicTacToe extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            xGoes: true
+        } 
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(e) {
+        const box = e.target.firstChild;
+        box.innerHTML = this.state.xGoes ? "X" : "O"
+        this.setState({xGoes: !this.state.xGoes})
+    }
+    render() {   
+    return (
+        <table onClick={this.handleClick}>
+            <tbody>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <Square key={1} number={0} />
+                <Square key={2} number={1} />
+                <Square key={3} number={2} />
             </tr>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <Square key={4} number={3} />
+                <Square key={5} number={4} />
+                <Square key={6} number={5} />
             </tr>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <Square key={7} number={6} />
+                <Square key={8} number={7} />
+                <Square key={9} number={8} />
             </tr>
+            </tbody>
         </table>
     )
+    }
 }
 
 export default TicTacToe
