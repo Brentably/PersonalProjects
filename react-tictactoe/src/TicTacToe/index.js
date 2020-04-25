6bc08ef7759a636e9b12e987c83988ea13e46579
brent <brent@pabnet.com>
2020-04-24 20:22:10 -0400

    added main functionality and you've won alert, more improvements to come

--- react-tictactoe/src/TicTacToe/index.js
+++ react-tictactoe/src/TicTacToe/index.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css'
 
 
 const TicTacToe = () => {
     const [xGoes, setXGoes] = useState(true)
     const [matrix, setMatrix] = useState({})
-    const switchTurns = () => {setXGoes(prevXGoes => !prevXGoes)}
-
-    const handleClear = () => {
-        setMatrix({})
-    }
+    const switchTurns = () => setXGoes(prevXGoes => !prevXGoes)
+    const handleClear = () => setMatrix({})
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+        ]
+    useEffect(() => {
+        for (let combo of winningCombos) {
+            if (combo.some(x => matrix[x] === undefined)) continue
+            if (matrix[combo[0]] === matrix[combo[1]] && matrix[combo[0]] === matrix[combo[2]]) alert("You've won!")
+        }
+        console.log(matrix)
+    })
 
     const Squares = []
     for (let x = 0; x < 9; x++) {
@@ -18,7 +32,7 @@ const TicTacToe = () => {
             number={x} 
             switchTurns={switchTurns} 
             xGoes={xGoes} 
-            letter={matrix[`${x}`]}
+            letter={matrix[x]}
             setMatrix={setMatrix}/>)
     }
     return (
@@ -51,7 +65,7 @@ const TicTacToe = () => {
 const Square = (props) => {
     const handleClick = () => {
         if (!props.letter) {
-            props.setMatrix(prevMatrix => ({...prevMatrix, [`${props.number}`]: props.xGoes? "X" : "O"}))
+            props.setMatrix(prevMatrix => ({...prevMatrix, [props.number]: props.xGoes? "X" : "O"}))
             props.switchTurns()
         }
     }
